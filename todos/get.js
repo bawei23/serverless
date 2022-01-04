1197b1e66098ededad2708b18c951c9c0c794af5
bawei23 <dicky.prasetya@energy-logistics.co.id>
2022-01-04 16:21:01 +0700

    deploy commit

--- todos/get.js
+++ todos/get.js
@@ -4,15 +4,30 @@ const AWS = require('aws-sdk');
 const dynamoDb = new AWS.DynamoDB.DocumentClient({region: 'us-east-2'});
 
 
-    module.exports.get= (event, callback) => {
-      const params = {
-        TableName: 'contact',
-      };
-    
-      return dynamoDb.scan(params, (error, data) => {
-        if (error) {
-          callback(error);
-        }
-        callback(error, data.Items);
-      });
-    };
+   	
+module.exports.get = (event, context, callback) => {
+  var params = {
+      TableName: "contact"
+  };
+
+
+  const onScan = (err, data) => {
+
+      if (err) {
+          console.log('Scan failed to load data. Error JSON:', JSON.stringify(err, null, 2));
+          callback(err);
+      } else {
+          console.log("Scan succeeded.");
+          return callback(null, {
+              statusCode: 200,
+              body: JSON.stringify({
+                  candidates: data.Items
+              })
+          });
+      }
+
+  };
+
+  dynamoDb.scan(params, onScan);
+
+};
