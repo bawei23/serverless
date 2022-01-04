c4a8a9e341b58a63e26635c7319732254586e3d1
bawei23 <dicky.prasetya@energy-logistics.co.id>
2022-01-04 17:42:28 +0700

    deploy commit

--- todos/create.js
+++ todos/create.js
@@ -8,57 +8,49 @@ const dynamoDb = new AWS.DynamoDB.DocumentClient({region: 'us-east-2'});
 
 	
 module.exports.create = (event, context, callback) => {
-  const requestBody = JSON.parse(event.body);
-  const fullname = requestBody.fullname;
-  const email = requestBody.email;
-  const phone_number = requestBody.phone_number;
- 
-  if (typeof fullname !== 'string' || typeof email !== 'string' || typeof phone_number !== 'number') {
+  const timestamp = new Date().getTime();
+  const data = JSON.parse(event.body);
+  if (typeof data.fullname !== 'string') {
     console.error('Validation Failed');
-    callback(new Error('Couldn\'t submit contact because of validation errors.'));
+    callback(null, {
+      statusCode: 400,
+      headers: { 'Content-Type': 'text/plain' },
+      body: 'Couldn\'t create the contact.',
+    });
     return;
   }
- 
-  submitContact(contactInfo(fullname, email, phone_number))
-    .then(res => {
-      callback(null, {
-        statusCode: 200,
-        body: JSON.stringify({
-          message: `Sucessfully submitted contact with email ${email}`,
-          contactID: res.contactID
-        })
-      });
-    })
-    .catch(err => {
-      console.log(err);
-      callback(null, {
-        statusCode: 500,
-        body: JSON.stringify({
-          message: `Unable to submit contact with email ${email}`
-        })
-      })
-    });
-};
- 
- 
-const submitContact = contact => {
-  console.log('Submitting contact');
-  const contactInfo = {
+
+  const params = {
     TableName: "contact",
-    Item: contact,
+    Item: {
+      contactID: uuid.v1(),
+      fullname: data.fullname,
+      email: data.email,
+      phone_number: data.phone_number,
+      checked: false,
+      createdAt: timestamp,
+      updatedAt: timestamp,
+    },
   };
-  return dynamoDb.put(contactInfo).promise()
-    .then(res => contact);
-};
- 
-const contactInfo = (fullname, email, phone_number) => {
-  const timestamp = new Date().getTime();
-  return {
-    contactID: uuid.v1(),
-    fullname: fullname,
-    email: email,
-    phone_number: phone_number,
-    submittedAt: timestamp,
-    updatedAt: timestamp,
-  };
+
+  // write the todo to the database
+  dynamoDb.put(params, (error) => {
+    // handle potential errors
+    if (error) {
+      console.error(error);
+      callback(null, {
+        statusCode: error.statusCode || 501,
+        headers: { 'Content-Type': 'text/plain' },
+        body: 'Couldn\'t create the contact.',
+      });
+      return;
+    }
+
+    // create a response
+    const response = {
+      statusCode: 200,
+      body: JSON.stringify(params.Item),
+    };
+    callback(null, response);
+  });
 };
